31dfbb582afe03a2789444af660b5f0855fd9a9b
e-rr <139189359+e-rr@users.noreply.github.com>
2024-02-20 14:23:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 def index():
   return(render_template("index.html"))
 
-@app.route("/main",methods=["GET","POST"]
+@app.route("/main",methods=["GET","POST"])
 def main();
-  name=request.form.get("name")
-  return(reader_templates("main.html"))
+  name = request.form.get("name")
+  return(reader_templates("main.html",r=name))
 
 if __name__ == "__main__":
   app.run()
