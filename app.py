4315e66195780b6357f475e236a82d63fd56affe
e-rr <139189359+e-rr@users.noreply.github.com>
2024-02-20 15:52:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,23 @@ from flask import Flask,request,render_template
 
 app = Flask(__name__)
 
+r = ""
+first_time=1
+
 @app.route("/",methods=["GET","POST"])
 def index():
   return(render_template("index.html"))
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-  r = request.form.get("r")
-  return(render_template("main.html",r=r))
+  global r,first_name
+  if first_name=1:
+    r = request.form.get("r")
+    first_name = 0
+    return(render_template("main.html",r=r))
 
 @app.route("/image_gpt",methods=["GET","POST"])
-def main():
+def image_gpt():
   return(render_template("image_gpt.html"))
 
 if __name__ == "__main__":
